ea3833a09d8ee13f2ae7f3570abf278e2a02f20f
DavidBallesterPirchmoser <al426684@uji.es>
2025-12-12 15:37:34 +0100

    camera_web

--- PlanUrFood/app/src/main/java/com/example/planurfood/ui/home/MainActivity.java
+++ PlanUrFood/app/src/main/java/com/example/planurfood/ui/home/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.planurfood.ui.home;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 
 import com.example.planurfood.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,10 +23,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.planurfood.databinding.ActivityMainBinding;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 private ActivityMainBinding binding;
+    Camera mCamera;
+    static File mediaStorageDir;
+    static File mediaFile;
+    private static final int REQUEST_CAMERA_WEB = 111;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,4 +75,85 @@ private ActivityMainBinding binding;
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    private android.hardware.Camera getCameraInstance() {
+        android.hardware.Camera camera = null;
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CAMERA_WEB);
+        }
+        try {
+            camera = android.hardware.Camera.open(0);
+        } catch (Exception e) {
+// cannot get camera or does not exist
+            Log.e("getCameraInstance", "ERROR" + e);
+        }
+        return camera;
+    }
+    private File getOutputMediaFile() {
+        if (mediaStorageDir == null){
+            mediaStorageDir = new File(this.getFilesDir(), "MyCameraApp");
+            if (!mediaStorageDir.exists()) {
+                if (!mediaStorageDir.mkdirs()) {
+                    Log.e("MyCameraApp", "failed to create directory");
+                    return null;
+                }
+            }
+        }
+        if (mediaFile==null) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG.jpg");
+        }
+        Log.v("ERROR", String.valueOf(mediaStorageDir));
+        Log.v("ERROR", String.valueOf(mediaFile));
+        return mediaFile;
+    }
+    public void captureCamera(){
+
+        // Cerrar cámara y volver a abrirla. O el Camera Preview
+        Log.v("CAMARA", mCamera.toString());
+        if (mCamera!=null) {
+            try {
+                mCamera.takePicture(null, null, mPicture);
+                Log.e("CAMARA", "capturando imagen");
+            } catch (Exception e) {
+                Log.e("Camera", "Demasiadas capturas");
+            }
+
+        }
+    }
+    byte[] resizeImage(byte[] input) {
+        Bitmap originalBitmap = BitmapFactory.decodeByteArray(input, 0, input.length);
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, 80, 107,
+                true);
+        ByteArrayOutputStream blob = new ByteArrayOutputStream();
+        resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 75, blob);
+        return blob.toByteArray();
+    }
+
+    android.hardware.Camera.PictureCallback mPicture = new android.hardware.Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, android.hardware.Camera camera) {
+            byte[] resized = resizeImage(data);
+            File pictureFile = getOutputMediaFile();
+            Log.v("ERROR_picture", String.valueOf(pictureFile));
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(resized);
+                fos.close();
+                mCamera.stopPreview();
+                mCamera.startPreview();
+                Log.e("Camera", "Camera_reiniciada");
+            } catch (Exception e) {
+                Log.e("onPictureTaken", "ERROR:" + e);
+            }
+        }
+    };
 }
